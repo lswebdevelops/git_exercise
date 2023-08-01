d0490ceab573040c6dfa9ac18967b8fd3f841d5e
lucasritzke <lucasritzke@gmail.com>
2023-08-01 10:43:11 -0300

    feat: Initial commit.

---
+++ array_function.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+#Inicio com array
+cores=("preto" "marrom" "rosa" "verde" "azul" "roxo" "amarelo" "vermelho" "laranja" "branco" "cinza" "lilas")
+#Inicio da função para mostrar todas as cores
+loop_color_print{
+
+}
+loop_color_print
