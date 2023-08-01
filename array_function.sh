b0302970355af34ca4aa4f2b620d7c3afc9fe912
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-08-01 10:49:10 -0300

    adding for loop

--- array_function.sh
+++ array_function.sh
@@ -4,8 +4,10 @@
 cores=("preto" "marrom" "rosa" "verde" "azul" "roxo" "amarelo" "vermelho" "laranja" "branco" "cinza" "lilas")
 #Inicio da função para mostrar todas as cores
 loop_color_print{
+    # adding for loop
 for color in "${colors[@]}"; do
         echo "$color"
     done
+  # adding for loop
 }
 loop_color_print
